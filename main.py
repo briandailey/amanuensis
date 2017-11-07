6a9ce80e445181d1f27717afe90de3256d09e57b
Brian Dailey <github@dailytechnology.net>
2017-11-07 17:55:52 -0600

    Moving to an object.

--- main.py
+++ main.py
@@ -12,129 +12,141 @@ ZENHUB_API_ROOT = 'https://api.zenhub.io'
 AUTOMATED_DESCRIPTION = 'Automatically created by Amanuensis.'
 
 
-def get_github_token():
-    token = ''
-    with open(CREDENTIALS_FILE, 'r') as fd:
-        token = fd.readline().strip()  # Can't hurt to be paranoid
-    return token
+class Amanuensis(object):
+    """ Because we need state, we have an object in which to put it. """
+
+    def __init__(org_slash_repo_name, start_date, end_date, *args, **kwargs):
+        org, repo = org_slash_repo_name.split('/')
+        self.org = org
+        self.repo_name = repo_name
+        self.start_date = start_date
+        self.end_date = end_date
+
+    @property
+    def github_token(self):
+        if not hasattr(self, '_github_token'):
+            with open(CREDENTIALS_FILE, 'r') as fd:
+                self._github_token = fd.readline().strip()  # Can't hurt to be paranoid
+        return self._github_token
+
+    @property
+    def zenhub_token(self):
+        if not hasattr(self, '_zenhub_token'):
+            with open(ZENHUB_CREDENTIALS_FILE, 'r') as fd:
+                self._zenhub_token = fd.readline().strip()  # Can't hurt to be paranoid
+        return self._zenhub_token
+
+    @property
+    def github_headers(self):
+        return {'Authorization':'token %s' % self.github_token}
+
+    def get_closed_issues(self, org, repo_name, start_date, end_date):
+        """ Return all issues from org/repo_name closed between start and end. """
+
+        # There's no easy way to get this (like the search query syntax) for private repos.
+        url = '{}/repos/{}/{}/issues'.format(GITHUB_API_ROOT, org, repo_name)
+        params = {'since': start_date + 'T00:00:00Z', 'state': 'closed', 'sort': 'updated', 'direction': 'asc', 'per_page': 100}
+        r = requests.get(url, params=params, headers=self.github_headers)
+
+        start_date_ts = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+        end_date_ts = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+
+        issues_returned = r.json()
+        print("Found {} matching issues. Pairing down.".format(len(issues_returned)))
+        matching_issues = []
+        for issue in issues_returned:
+            if 'pull_request' in issue:
+                # We don't want pull requests and there's
+                # not a great way to filter them out.
+                continue
+            closed_at = datetime.datetime.strptime(issue['closed_at'], '%Y-%m-%dT%H:%M:%SZ')
+            if start_date_ts < closed_at < end_date_ts:
+                matching_issues.append(issue)
+
+        return matching_issues
+
+    @property
+    def repo_id(self):
+        if not hasattr(self, '_repo_id')
+            # First, we have to get the repo id.
+            url = '{}/repos/{}/{}'.format(GITHUB_API_ROOT, org, repo_name)
+            headers = {'Authorization':'token %s' % get_github_token()}
+
+            r = requests.get(url, headers=headers)
+
+            self._repo_id = r.json()['id']
+        return self._repo_id
+
+    def get_milestone(org, repo_name, start_date, end_date):
+        """ Find the milestone number with the due date and name we are looking for.
+        If it doesn't exist, return None. """
+
+        matching_milestone = None
+        automated_title = '{} - {}'.format(start_date, end_date)
+
+        url = '{}/repos/{}/{}/milestones'.format(GITHUB_API_ROOT, org, repo_name)
+        params = {'state': 'open', 'sort': 'due_on', 'direction': 'desc'}
+
+        r = requests.get(url, params=params, headers=self.github_headers)
+
+        # Note that if you set due_on to YYYY-MM-DDT00:00:00 you will actually
+        # end up with due_on being 07:00:00 the day prior. So look for that!
+        due_on = (datetime.datetime.strptime(end_date, '%Y-%m-%d') - datetime.timedelta(days=1)).strftime('%Y-%m-%d') + 'T07:00:00Z'
+
+        for milestone in r.json():
+            if milestone['due_on'] == due_on and \
+                milestone['title'] == automated_title and \
+                milestone['description'] == AUTOMATED_DESCRIPTION:
+                matching_milestone = milestone
+
+        return matching_milestone
+
+    def get_or_create_milestone(org, repo_name, start_date, end_date):
+        milestone = get_milestone(org, repo_name, start_date, end_date)
+        if not milestone:
+            milestone = create_milestone(org, repo_name, start_date, end_date)
+        return milestone
+
+    def create_milestone(org, repo_name, start_date, end_date):
+        url = '{}/repos/{}/{}/milestones'.format(GITHUB_API_ROOT, org, repo_name)
+        data = {
+            'title': '{} - {}'.format(start_date, end_date),
+            'state': 'open',
+            'description': AUTOMATED_DESCRIPTION,
+            'due_on': end_date + 'T00:00:00Z',
+        }
+        headers = {'Authorization':'token %s' % get_github_token()}
+
+        r = requests.post(url, json=data, headers=headers)
+
+        return r.json()
+
+    def set_issue_milestone(org, repo_name, issue_number, milestone_number):
+        url = '{}/repos/{}/{}/issues/{}'.format(GITHUB_API_ROOT, org, repo_name, issue_number)
+        headers = {'Authorization':'token %s' % get_github_token()}
+        data = {'milestone': milestone_number}
+
+        requests.patch(url, json=data, headers=headers)
+
+    @property
+    def zenhub_headers(self):
+        return {'X-Authentication-Token': self.zenhub_token}
+
+    def set_milestone_start_date(org, repo_name, milestone_number, start_date):
+        """ Set the milestone start date on the ZenHub side. """
+
+        url = '{zenhub_api_root}/p1/repositories/{repo_id}/milestones/{milestone_number}/start_date'.format(
+            zenhub_api_root=ZENHUB_API_ROOT,
+            repo_id=self.repo_id,
+            milestone_number=milestone_number)
+        # Gotta add the time offset.
+        data = {'start_date': start_date + 'T07:00:00Z'}
+        r = requests.post(url, data=data, headers=self.zenhub_headers)
+        return r
 
 
-def get_zenhub_token():
-    token = ''
-    with open(ZENHUB_CREDENTIALS_FILE, 'r') as fd:
-        token = fd.readline().strip()  # Can't hurt to be paranoid
-    return token
-
-
-def get_closed_issues(org, repo_name, start_date, end_date):
-    """ Return all issues from org/repo_name closed between start and end. """
-
-    # There's no easy way to get this (like the search query syntax) for private repos.
-    url = '{}/repos/{}/{}/issues'.format(GITHUB_API_ROOT, org, repo_name)
-    params = {'since': start_date + 'T00:00:00Z', 'state': 'closed', 'sort': 'updated', 'direction': 'asc', 'per_page': 100}
-    headers = {'Authorization':'token %s' % get_github_token()}
-
-    r = requests.get(url, params=params, headers=headers)
-
-    start_date_ts = datetime.datetime.strptime(start_date, '%Y-%m-%d')
-    end_date_ts = datetime.datetime.strptime(end_date, '%Y-%m-%d')
-
-    issues_returned = r.json()
-    print("Found {} matching issues. Pairing down.".format(len(issues_returned)))
-    matching_issues = []
-    for issue in issues_returned:
-        if 'pull_request' in issue:
-            # We don't want pull requests.
-            continue
-        closed_at = datetime.datetime.strptime(issue['closed_at'], '%Y-%m-%dT%H:%M:%SZ')
-        if start_date_ts < closed_at < end_date_ts:
-            matching_issues.append(issue)
-
-    return matching_issues
-
-
-def get_milestone(org, repo_name, start_date, end_date):
-    """ Find the milestone number with the due date and name we are looking for.
-    If it doesn't exist, return None. """
-
-    matching_milestone = None
-    automated_title = '{} - {}'.format(start_date, end_date)
-
-    url = '{}/repos/{}/{}/milestones'.format(GITHUB_API_ROOT, org, repo_name)
-    params = {'state': 'open', 'sort': 'due_on', 'direction': 'desc'}
-    headers = {'Authorization':'token %s' % get_github_token()}
-
-    r = requests.get(url, params=params, headers=headers)
-
-    # Note that if you set due_on to YYYY-MM-DDT00:00:00 you will actually
-    # end up with due_on being 07:00:00 the day prior. So look for that!
-    due_on = (datetime.datetime.strptime(end_date, '%Y-%m-%d') - datetime.timedelta(days=1)).strftime('%Y-%m-%d') + 'T07:00:00Z'
-
-    for milestone in r.json():
-        if milestone['due_on'] == due_on and \
-            milestone['title'] == automated_title and \
-            milestone['description'] == AUTOMATED_DESCRIPTION:
-            matching_milestone = milestone
-
-    return matching_milestone
-
-
-def get_or_create_milestone(org, repo_name, start_date, end_date):
-    milestone = get_milestone(org, repo_name, start_date, end_date)
-    if not milestone:
-        milestone = create_milestone(org, repo_name, start_date, end_date)
-    return milestone
-
-def create_milestone(org, repo_name, start_date, end_date):
-    url = '{}/repos/{}/{}/milestones'.format(GITHUB_API_ROOT, org, repo_name)
-    data = {
-        'title': '{} - {}'.format(start_date, end_date),
-        'state': 'open',
-        'description': AUTOMATED_DESCRIPTION,
-        'due_on': end_date + 'T00:00:00Z',
-    }
-    headers = {'Authorization':'token %s' % get_github_token()}
-
-    r = requests.post(url, json=data, headers=headers)
-
-    return r.json()
-
-
-def set_issue_milestone(org, repo_name, issue_number, milestone_number):
-    url = '{}/repos/{}/{}/issues/{}'.format(GITHUB_API_ROOT, org, repo_name, issue_number)
-    headers = {'Authorization':'token %s' % get_github_token()}
-    data = {'milestone': milestone_number}
-
-    requests.patch(url, json=data, headers=headers)
-
-
-def set_milestone_start_date(org, repo_name, milestone_number, start_date):
-    """ Set the milestone start date on the ZenHub side. """
-
-    try:
-        zenhub_token = get_zenhub_token()
-    except FileNotFoundError:
-        print("No ZenHub token found, not setting milestone start date.")
-        return
-
-    # First, we have to get the repo id.
-    url = '{}/repos/{}/{}'.format(GITHUB_API_ROOT, org, repo_name)
-    headers = {'Authorization':'token %s' % get_github_token()}
-
-    r = requests.get(url, headers=headers)
-
-    repo_id = r.json()['id']
-
-    url = '{zenhub_api_root}/p1/repositories/{repo_id}/milestones/{milestone_number}/start_date'.format(
-        zenhub_api_root=ZENHUB_API_ROOT,
-        repo_id=repo_id,
-        milestone_number=milestone_number)
-    headers = {'X-Authentication-Token': zenhub_token}
-    # Gotta add the time offset.
-    data = {'start_date': start_date + 'T07:00:00Z'}
-    r = requests.post(url, data=data, headers=headers)
-    return r
-
+    def get_issue_zenhub_data(issue_number):
+        pass
 
 def run(days, repo, token, date, force):
     # Get start and end date.
@@ -170,6 +182,7 @@ def run(days, repo, token, date, force):
 @click.option('--token', '-t', help="Your GitHub token (optional, can place in .token file).")
 @click.option('--force', '-f', is_flag=True, help="Even if the issues are attached to another milestone, move them.")
 def cli(days, repo, token, date, force):
+    amanuensis = Amanuensis()
     run(days, repo, token, date, force)
 
 

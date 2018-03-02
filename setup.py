91c01894ce52c9a3d6da4156aad6b30583aada18
Brian Dailey <git@dailytechnology.net>
2018-03-02 13:56:57 -0600

    Package up that baby!

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='amanuensis',
+      version='0.1',
+      description='Gather GH issues and put into ZH milestone.',
+      url='http://github.com/briandailey/amanuensis',
+      author='Brian Dailey',
+      author_email='github@dailytechnology.net',
+      license='MIT',
+      packages=['amanuensis'],
+      install_requires=[
+          'requests==2.18.4',
+          'click==6.7',
+          'urllib3==1.22',
+      ],
+      # scripts=['bin/amanuensis', ]
+      entry_points={
+          'console_scripts': ['amanuensis=amanuensis.amanuensis:cli'],
+      },
+      zip_safe=False)
